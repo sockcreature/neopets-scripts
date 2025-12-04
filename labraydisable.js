782b860b092df495f245c4dbceb5697e27c77651
Carolyn <sockcreature@gmail.com>
2025-12-03 22:31:52 -0800

    Clean up metadata in labraydisable.js
    Removed unnecessary metadata from the user script.

--- labraydisable.js
+++ labraydisable.js
@@ -1,12 +1,9 @@
 // ==UserScript==
 // @name         Lab Ray Button Disable
-// @namespace    http://tampermonkey.net/
 // @version      2025-12-04
 // @description  block certain pets from being selected in lab ray
 // @author       Socks
 // @match        https://www.neopets.com/lab2.phtml
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
 // ==/UserScript==
 
 (function() {
